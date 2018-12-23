1d8966cd27de2080b845c597cd55f8e2a7502b1a
LAPTOP-A64JB3CQ\ET-MARLABS <grandhi.aditya@etmarlabs.com>
2018-12-23 13:21:39 +0530

    new commit

---
+++ .sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,24 @@
+declare module "@salesforce/contentAssetUrl/Cookpatternv1" {
+    var Cookpatternv1: string;
+    export default Cookpatternv1;
+}
+declare module "@salesforce/contentAssetUrl/Cookpatternv11" {
+    var Cookpatternv11: string;
+    export default Cookpatternv11;
+}
+declare module "@salesforce/contentAssetUrl/Cookpatternv2" {
+    var Cookpatternv2: string;
+    export default Cookpatternv2;
+}
+declare module "@salesforce/contentAssetUrl/recipes_sq_logo" {
+    var recipes_sq_logo: string;
+    export default recipes_sq_logo;
+}
+declare module "@salesforce/contentAssetUrl/recipeslogo600x120" {
+    var recipeslogo600x120: string;
+    export default recipeslogo600x120;
+}
+declare module "@salesforce/contentAssetUrl/recipeslogo600x1201" {
+    var recipeslogo600x1201: string;
+    export default recipeslogo600x1201;
+}
